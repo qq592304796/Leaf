a54022f09bcd8f3ef17c8e510cd84629568a2b5b
qq592304796 <592304796@qq.com>
2019-07-08 16:22:58 +0800

    feat(leaf-core|leaf-server):集成redis作为cache-id，新增批量获取id以及获取id号段接口，调整java编译版本为1.8，以及补充相关单元测试

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafController.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafController.java
@@ -4,12 +4,17 @@ import com.sankuai.inf.leaf.common.Result;
 import com.sankuai.inf.leaf.common.Status;
 import com.sankuai.inf.leaf.server.exception.LeafServerException;
 import com.sankuai.inf.leaf.server.exception.NoKeyException;
+import com.sankuai.inf.leaf.server.model.SegmentDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class LeafController {
@@ -18,6 +23,9 @@ public class LeafController {
     SegmentService segmentService;
     @Autowired
     SnowflakeService snowflakeService;
+
+    @Autowired
+    private CacheService cacheService;
 
     @RequestMapping(value = "/api/segment/get/{key}")
     public String getSegmentID(@PathVariable("key") String key) {
@@ -41,5 +49,49 @@ public class LeafController {
             throw new LeafServerException(result.toString());
         }
         return String.valueOf(result.getId());
+    }
+
+    @RequestMapping(value = "/api/segment/batchGet/{key}")
+    public List<String> batchGetSegmentID(@PathVariable("key") String key, @RequestParam Integer batchSize) {
+        List<String> segmentIds = new ArrayList<>(batchSize);
+        for (int i = 0; i < batchSize; i++) {
+            segmentIds.add(get(key, segmentService.getId(key)));
+        }
+        return segmentIds;
+    }
+
+    @RequestMapping(value = "/api/segment/get")
+    public SegmentDTO getSegment(@RequestParam String key, @RequestParam Integer step) {
+        // 可能获取的id不是连续的，不改动内部实现的逻辑，只返回连续的号段
+        long maxId = 0L;
+        int currentStep = 0;
+        for (; currentStep < step; ) {
+            Result result = segmentService.getId(key);
+            // 与项目保持一致，如果出现异常抛出
+            if (result.getStatus().equals(Status.EXCEPTION)) {
+                throw new LeafServerException(result.toString());
+            }
+            long id = result.getId();
+            long difference = id - maxId;
+            if (maxId != 0 && difference != 1) {
+                break;
+            }
+            maxId = id;
+            currentStep++;
+        }
+        SegmentDTO segment = new SegmentDTO();
+        segment.setMaxId(maxId);
+        segment.setStep(currentStep);
+        return segment;
+    }
+
+    @RequestMapping(value = "/api/cache/get")
+    public Long getCacheID(@RequestParam String key) {
+        return cacheService.getId(key);
+    }
+
+    @RequestMapping(value = "/api/cache/getWithExpired")
+    public Long getCacheIDWithExpired(@RequestParam String key) {
+        return cacheService.getIdWithExpired(key);
     }
 }
---
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/cache/CacheConstant.java
@@ -0,0 +1,20 @@
+package com.sankuai.inf.leaf.server.cache;
+
+/**
+ * Cahce服务常量
+ * @author jiangxinjun
+ * @date 2019/07/05
+ */
+public class CacheConstant {
+
+    /**
+     * 不过期增量ID KEY前缀
+     */
+    public final static String INCREMENT_ID_HOWEVER_KEY_PREFIX = "INCREMENT_ID_HOWEVER:";
+
+    /**
+     * 过期增量ID KEY前缀
+     */
+    public final static String INCREMENT_ID_EXPIRED_KEY_PREFIX = "INCREMENT_ID_EXPIRED:";
+
+}
