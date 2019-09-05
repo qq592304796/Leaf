cb9902942ef8e6355e1f3308777ac78724230c2e
qq592304796 <592304796@qq.com>
2019-09-05 11:06:55 +0800

    build:修改配置脚本

--- leaf-server/bin/dev_configure.sh
+++ leaf-server/bin/dev_configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 打包前操作 （替换配置）
 #curl 	 xxxxxx >xxxx.yml
-sed -r 's/^(active: )(\w+)$/\1dev/g' src/main/resources/application.yml
+sed -r -n -i 's/(active: )(\w+)/\1dev/g' leaf-server/src/main/resources/application.yml
