efbf35ce41834f3d25d0f9477a2137a0ff614582
qq592304796 <592304796@qq.com>
2019-09-18 11:27:19 +0800

    build:测试环境redis配置去掉密码

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafServerApplication.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafServerApplication.java
@@ -3,10 +3,14 @@ package com.sankuai.inf.leaf.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author jiangxinjun
+ * @date 2019/09/18
+ */
 @SpringBootApplication
 public class LeafServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LeafServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LeafServerApplication.class, args);
+    }
 }
